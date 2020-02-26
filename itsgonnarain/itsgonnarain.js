2648f54532d46e156e06715b5d4e6acdaf18e47b
Evan Savage <evan@savageinter.net>
2020-02-26 08:28:58 -0500

    Phase-shifted stereo implementation
    Couldn't get `createStereoPanner()` to work, so mimicked it with
    `createPanner()` and SOME MATH.

--- itsgonnarain/itsgonnarain.js
+++ itsgonnarain/itsgonnarain.js
@@ -1,9 +1,45 @@
 console.log("It's gonna rain");
 
-async function main() {
-  const response = await fetch('gino_soccio_remember_loop.mp3');
+let audioContext = new AudioContext();
+
+async function loadAudio(url) {
+  const response = await fetch(url);
   const arrayBuffer = await response.arrayBuffer();
-  console.log('Received', arrayBuffer);
+  return audioContext.decodeAudioData(arrayBuffer);
+}
+
+function setStereoPan(pannerNode, pan) {
+  var xDeg = pan * 45;
+  var zDeg = xDeg + 90;
+  if (zDeg > 90) {
+    zDeg = 180 - zDeg;
+  }
+  var x = Math.sin(xDeg * (Math.PI / 180));
+  var z = Math.sin(zDeg * (Math.PI / 180));
+  pannerNode.setPosition(x, 0, z);
+}
+
+function startAudioLoop(audioBuffer, pan = 0, rate = 1) {
+  const sourceNode = audioContext.createBufferSource();
+
+  sourceNode.buffer = audioBuffer;
+  sourceNode.loop = true;
+  sourceNode.playbackRate.value = rate;
+
+  const pannerNode = audioContext.createPanner();
+  setStereoPan(pannerNode, pan);
+
+  sourceNode.connect(pannerNode);
+  pannerNode.connect(audioContext.destination);
+
+  sourceNode.start(0);
+}
+
+async function main() {
+  const audioBuffer = await loadAudio('gino_soccio_remember_loop.mp3');
+  console.log('Decoded', audioBuffer);
+  startAudioLoop(audioBuffer, -1, 1);
+  startAudioLoop(audioBuffer, 1, 1.002);
 }
 
 main();
